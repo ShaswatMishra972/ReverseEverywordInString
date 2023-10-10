b9dfc4448300a9d95e011bfbb688fa96fa84815b
ShaswatMishra <147424330+ShaswatMishra972@users.noreply.github.com>
2023-10-10 20:46:30 +0530

    Add files via upload

---
+++ ReverseSentence.js
@@ -0,0 +1,6 @@
+const string = "INDIA IS MY COUNTRY  AND ALL INDIANS ARE MY BROTHER AND SISTER.";
+console.log("INPUT STRING: ", string);
+
+const result = string.split("").reverse().join("");
+
+console.log("AFTER REVERSE: ", result);
